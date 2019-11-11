77523e8251602b6d5e5ebb3676ab41de39a3733f
JackWei <wdy410182@163.com>
2019-11-11 15:46:30 +0800

    第一次提交。

---
+++ src/main/java/com/next/FilmmallApplication.java
@@ -0,0 +1,13 @@
+package com.next;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmmallApplication.class, args);
+	}
+
+}
